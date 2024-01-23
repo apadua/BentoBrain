d52a953830de8e9cb5c63b3cca766a0a4d9fe516
Andre Padua <ampadua@yahoo.com>
2024-01-22 20:16:42 -0500

    Updated config.h

--- config.h
+++ config.h
@@ -2,17 +2,22 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// WiFi credentials
+// Replace with your WIFI Network name and Password
 const char* ssid = "your_SSID";
 const char* password = "your_PASSWORD";
 
-// MQTT Broker settings
-const char* mqtt_broker = "broker_address";
-const char* mqtt_password = "your_password";
+// Use Printer IP Address and LAN Password grabbed from the LCD screen
+const char* mqtt_broker = "printer_address";
+const char* mqtt_password = "printer_lan_password";
 
-
-//Fan Control Settings
+// Other Adjustments 
 const int fanPin = 6; // PWM pin for the fan is connected to pin 6
-const float tempThreshold = 200.0; // Temperature threshold for fan control. 200 triggers fan ON for any filament type
+const float tempThreshold = 180.0; // Temperature threshold for fan control. 180 will trigger fan ON for any filament type
 const unsigned long fanOffDelay = 300000; // Delay duration before fan shutoff in milliseconds (300000 is 5 minutes)
-#endif
+
+
+
+// Only change these if you know what you're doing
+const char* topic = "device/#";
+const char* mqtt_username = "bblp";
+const int mqtt_port = 8883; // MQTT port (usually 8883 for SSL)
