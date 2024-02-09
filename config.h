4ee3a8507fffde111ded1e549179a296fb84b24a
Andre Padua <ampadua@yahoo.com>
2024-02-09 00:53:35 -0500

    Update config.h
    Cleaned up files and notes.

--- config.h
+++ config.h
@@ -1,23 +1,11 @@
-// config.h
-#ifndef CONFIG_H
-#define CONFIG_H
-
-// Replace with your WIFI Network name and Password
-const char* ssid = "your_SSID";
-const char* password = "your_PASSWORD";
-
-// Use Printer IP Address and LAN Password grabbed from the LCD screen
-const char* mqtt_broker = "printer_address";
-const char* mqtt_password = "printer_lan_password";
-
-// Other Adjustments 
-const int fanPin = 6; // PWM pin for the fan is connected to pin 6
-const float tempThreshold = 180.0; // Temperature threshold for fan control. 180 will trigger fan ON for any filament type
-const unsigned long fanOffDelay = 300000; // Delay duration before fan shutoff in milliseconds (300000 is 5 minutes)
-
-
-
-// Only change these if you know what you're doing
-const char* topic = "device/#";
-const char* mqtt_username = "bblp";
-const int mqtt_port = 8883; // MQTT port (usually 8883 for SSL)
+char ssid[32] = "wifi_ssid";
+char password[64] = "wifi_password";
+char mqtt_broker[64] = "192.168.1.66"; //3d Printer 
+char topic[64] = "device/+/report";
+char mqtt_username[32] = "bblp";
+char mqtt_password[32] = "<password>"; 3d Printer password obtained from LCD screen LAN Management
+int mqtt_port = 8883;
+int fanPin = 6;
+float tempThreshold = 180.0;
+unsigned long fanOffDelay = 300000;
+String authToken = "-1";
